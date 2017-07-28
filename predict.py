ed45967fca871a5c793ed24db4a448ca889a3bac
kefth <kefth@users.noreply.github.com>
2017-07-28 17:52:27 +0200

    Minor changes

--- predict.py
+++ predict.py
@@ -78,4 +78,4 @@ if __name__ == '__main__':
     pred = predict(net, test_loader)
     labels, _, _ = data.get_labels('data/labels.txt')
     pred = pd.DataFrame(pred.numpy(), columns = labels)
-    print(pred.top())
+    print(pred.head())
